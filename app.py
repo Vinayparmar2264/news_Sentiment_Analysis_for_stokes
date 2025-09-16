7a3dc21768bfafc644768c908290df2eec129991
Vinayparmar2264 <vinayparmar2264@gmail.com>
2025-09-17 00:54:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -264,6 +264,7 @@ def general_news():
         return jsonify({"error": str(e)}), 500
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+# if __name__ == "__main__":
+#     app.run(debug=True, host="0.0.0.0")
+
 
